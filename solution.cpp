28c0b80cba5d2bf4aba251ff8e077d5009f7b850
Steven Lin <steven_lx@outlook.com>
2022-06-13 14:27:05 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -14,7 +14,9 @@ using namespace sol1209;
 using namespace std;
 
 /*takeaways
-  - use stack
+  - scan the array exactly once
+  - use a hash table where the key is the value of the number
+    and value is the index if the number
 */
 
 vector<int> Solution::twoSum(vector<int> &nums, int target)
@@ -24,6 +26,14 @@ vector<int> Solution::twoSum(vector<int> &nums, int target)
 
   for (auto i = 0; i < nums.size(); i++)
   {
+    /* since there will be exactly one solution
+       - {a,b}
+       - one of them will get inserted first
+         let's say it's a
+       -  when we scan in b, we can then
+         exit as a+b = target or [target-b]
+         exists in the hash table
+    */
     if (m.count(target - nums[i]) > 0)
       return {m[target - i], i};
     m[nums[i]] = i;
